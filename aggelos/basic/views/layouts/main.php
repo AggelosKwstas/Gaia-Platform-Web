1a4cce4d4969f9f2e996944f31972438a71b9ef4
unknown <agelos.kostas21@gmail.com>
2023-03-13 10:23:52 +0200

    dont remember

--- aggelos/basic/views/layouts/main.php
+++ aggelos/basic/views/layouts/main.php
@@ -25,7 +25,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>GAIA V2</title>
-    <link rel="icon" type="image/x-icon" href="asset/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="asset/logo.png" />
     <?php $this->head() ?>
 </head>
 <body id="page-top">
@@ -51,63 +51,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
         </div>
     </div>
 </nav>
-<!-- Mashead header-->
-<header class="masthead">
-    <div class="container px-5" style="margin-right: 10em">
-        <div class=" gx-5 align-items-center">
-            <div class="col-lg">
-                <!-- Mashead text and app badges-->
-                <div class="mb-5 mb-lg-0 text-center text-lg-start">
-                    <h1 class="display-2 lh-1 mb-3">Create powerful IoT networks, We’ve designed everything beyond...
-                    </h1>
-                    <img class="main_logo" src="asset/sensors.jpg" style="height: 200px;width: 95%">
-                    <div style="text-align: center">
-
-                        <div id="wrap">
-                            <a href="#" class="btn-slide2">
-                                <span class="circle2"><i class="fa fa-download"></i></span>
-                                <span class="title2">Download GAIA Desktop Application</span>
-                                <span class="title-hover2">Click here</span>
-                            </a>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</header>
-
-<section class="bg-light">
-   <div class="container" style="text-align: center">
-       <div class="row">
-           <div class="col offset-md-2 ">
-               <h5><b>POWERFULL SENSOR NETWORKS</b></h5>
-               <p>
-                   Fast installation and minimum maintenance costs. Diverse services and applications. Robust equipment and network scalability.
-               </p>
-           </div>
-           <div class="col">
-               <h5><b>OVER 50 SENSOR TYPES</b></h5>
-               <p>
-                   Sensors for measuring air quality, water quality, ions concentration, container levels, structural health, noise, agriculture parameters, security parameters, radioactivity levels etc.
-               </p>
-           </div>
-           <div class="col">
-               <h5><b>SOLAR POWERED</b></h5>
-               <p>
-                   Each sensor node has a battery that can be recharged using an internal or external solar panel. Batteries have a 6600mAh load that ennsures non-stop working time, even in days with no sunlight at all.
-               </p>
-           </div>
-       </div>
-   </div>
-</section>
-<!-- Call to action section-->
-<section class="cta" id="map-container">
-<!--    <div id="map"></div>-->
-</section>
-
-
+<?php echo $content?>
 <!-- Footer-->
 <footer class="bg-black text-center py-3">
     <div class="container px-5">
