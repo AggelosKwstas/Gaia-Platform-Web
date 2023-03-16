b458b15d0f1cbc934b95de86a76421310066a70f
tasosvittas <tasosvittas@gmail.com>
2023-03-16 14:24:01 +0200

    front

--- tasos/GaiaPlatform/views/layouts/main.php
+++ tasos/GaiaPlatform/views/layouts/main.php
@@ -1,148 +1,81 @@
 <?php
 
 /** @var yii\web\View $this */
+
 /** @var string $content */
 
 use app\assets\AppAsset;
-use rmrevin\yii\fontawesome\FA;
 
 AppAsset::register($this);
-rmrevin\yii\fontawesome\AssetBundle::register($this);
 //$this->registerCsrfMetaTags();
+
 $this->registerMetaTag(['charset' => Yii::$app->charset], 'charset');
-$this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1, shrink-to-fit=no']);
+$this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, initial-scale=1, shrink-to-fit=yes']);
 $this->registerMetaTag(['name' => 'description', 'content' => $this->params['meta_description'] ?? '']);
 $this->registerMetaTag(['name' => 'keywords', 'content' => $this->params['meta_keywords'] ?? '']);
-$this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii::getAlias('@web/favicon.ico')]);
+$this->registerLinkTag(['rel' => 'icon', 'type' => 'image/png', 'href' => 'asset/logo.png']);
 ?>
 <?php $this->beginPage() ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>GAIA V2</title>
-    <link rel="icon" type="image/x-icon" href="asset/logo.png" />
-    <?php $this->head() ?>
-</head>
-<body id="page-top">
-<?php $this->beginBody() ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <meta name="description" content=""/>
+        <meta name="author" content=""/>
+        <title>GAIA V2</title>
+        <link rel="icon" type="image/x-icon" href="asset/logo.png"/>
+        <?php $this->head() ?>
+    </head>
+    <body id="page-top">
+    <?php $this->beginBody() ?>
 
-<!-- Navigation-->
-<nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
-    <div class="container px-5">
-        <a class="navbar-brand fw-bold" href="#page-top"><img class="main_logo" src="asset/GaiaNewLogoEarth.png" style="height: 75px!important;"> <h id="h_logo">GAIA PLATFORM</h></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            Menu
-            <i class="bi-list"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
-                <li class="nav-item"><a class="nav-link me-lg-3" href="#map-container"><b>Station Overview</b></a></li>
-                <li class="nav-item"><a class="nav-link me-lg-3" href="#map"><b>Map</b></a></li>
-            </ul>
-<!--            <button class="btn btn-primary rounded-pill px-3 mb-2 mb-lg-0" data-bs-toggle="modal" href="login.php">-->
-<!--              <a data-method="post" class="btn btn-primary rounded-pill px-3 mb-2 mb-lg-0" href="--><?php ////echo \yii\helpers\Url::to(['site/login'])?><!--" />-->
-<!--                        <span class="d-flex align-items-center"><i class="fa fa-sign-in pull-left"></i><span class="small">Login</span>-->
-<!--                        </span>-->
-<!--            </button>-->
-            <a
-               class="btn btn-primary"
-               href="<?php echo \yii\helpers\Url::to(['admin/login']) ?>">Login</a>
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
+        <div class="container px-5">
+            <a class="navbar-brand fw-bold" href="<?php
+            if (\yii\helpers\Url::current() == \yii\helpers\Url::to(['site/index'])) {
+                echo '#page-top';
+            } else
+                echo \yii\helpers\Url::to(['site/index']);
+            ?>"><img class="main_logo"
+                     src="asset/logo.png"
+                     style="height: 75px!important;">
+                <h id="h_logo">GAIA PLATFORM</h>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+                    aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="bi-list"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
+                    <li class="nav-item"><a class="nav-link <?php
+                        if (\yii\helpers\Url::current() == \yii\helpers\Url::to(['site/map']))
+                            echo 'active ';
+                        ?>me-lg-3"
+                                            href="<?php echo \yii\helpers\Url::to(['site/map']) ?>"><b>Map</b></a></li>
+                </ul>
+                <button class="btn btn-primary rounded-pill px-3 mb-2 mb-lg-0" data-bs-toggle="modal"
+                        data-bs-target="#feedbackModal">
+                        <span class="d-flex align-items-center"><i style="display: block;text-align: left" class="fa-solid fa-right-to-bracket"></i>&nbsp;<span
+                                    class="small">Login</span>
+                        </span>
+                </button>
+            </div>
         </div>
-    </div>
-</nav>
-<?php echo $content?>
-<!-- Footer-->
-<footer class="bg-black text-center py-3">
-    <div class="container px-5">
-        <div class="text-white-50 small">
-            <div class="mb-2">&copy; Neuron Energy Solutions 2023. All Rights Reserved.</div>
-            <div class="mb-2">Powered By:</div>
-            <img src="asset/logoNE.png">
-            <img src="asset/zitsa.png">
-            <div class="mb-2">Follow us for more:</div>
-            <a href="https://www.neuronenergy.com/"><i class="fa fa-globe" style="font-size: 40px"></i></a>
-            <span class="mx-1">&nbsp;&nbsp;</span>
-            <a href="https://twitter.com/SolarEye_PV"><i class="fa fa-twitter" style="font-size: 40px"></i></a>
-            <span class="mx-1">&nbsp;&nbsp;</span>
-            <a href="https://www.facebook.com/profile.php?id=100051469122856"><i class="fa fa-facebook" style="font-size: 40px"></i></a>
+    </nav>
+    <?php echo $content ?>
+    <!-- Footer-->
+    <footer class="bg-black text-center py-1">
+        <div class="container px-5">
+            <div class="text-white-50 small mt-2">
+                <div class="mb-2">&copy; Neuron Energy Solutions 2023. All Rights Reserved.</div>
+                <a href="https://www.neuronenergy.com/"><img class="footer_logo" style="height: 60px;" src="asset/logoNE.png"></a>
         </div>
-    </div>
-</footer>
-
-<!-- Feedback Modal-->
-<!--<div class="modal fade" id="feedbackModal" tabindex="-1" aria-labelledby="feedbackModalLabel" aria-hidden="true">-->
-<!--    <div class="modal-dialog modal-dialog-centered">-->
-<!--        <div class="modal-content">-->
-<!--            <div class="modal-header bg-white p-4">-->
-<!--                <h5 class="modal-title font-alt" id="feedbackModalLabel" style="color: #5caa32">Login</h5>-->
-<!--                <button class="btn-close btn-close-white" type="button" data-bs-dismiss="modal" aria-label="Close"></button>-->
-<!--            </div>-->
-<!--            <div class="modal-body border-0 p-4">-->
-<!--                <form id="contactForm" data-sb-form-api-token="API_TOKEN">-->
-<!--                     Name input-->
-<!--                    <div class="form-floating mb-3" >-->
-<!--                        <input class="form-control" id="firstname" type="text" style="border-radius: 100px;" placeholder="Enter your firstname..." data-sb-validations="required" />-->
-<!--                        <label for="firstname">Firstname</label>-->
-<!--                        <div class="invalid-feedback" data-sb-feedback="firstname:required">Firstname is required.</div>-->
-<!--                    </div>-->
-<!--                    lastname-->
-<!--                    <div class="form-floating mb-3">-->
-<!--                        <input class="form-control" id="lastname" type="text" style="border-radius: 100px;" placeholder="Enter your lastname here..." style="height: 10rem" data-sb-validations="required" />-->
-<!--                        <label for="lastname">Lastname</label>-->
-<!--                        <div class="invalid-feedback" data-sb-feedback="lastname:required">Lastname is required.</div>-->
-<!--                    </div>-->
-                    <!-- Email address input-->
-<!--                    <div class="form-floating mb-3">-->
-<!--                        <input class="form-control" id="email" type="email" style="border-radius: 100px;" placeholder="name@example.com" data-sb-validations="required,email" />-->
-<!--                        <label for="email">Email address</label>-->
-<!--                        <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>-->
-<!--                        <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>-->
-<!--                    </div>-->
-                    <!-- Phone number input-->
-<!--                    <div class="form-floating mb-3">-->
-<!--                        <input class="form-control" id="phone" type="tel" style="border-radius: 100px;" placeholder="(123) 456-7890" data-sb-validations="required" />-->
-<!--                        <label for="phone">Phone number</label>-->
-<!--                        <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>-->
-<!--                    </div>-->
-
-                    <!-- Submit success message-->
-                    <!---->
-                    <!-- This is what your users will see when the form-->
-                    <!-- has successfully submitted-->
-<!--                    <div class="d-none" id="submitSuccessMessage">-->
-<!--                        <div class="text-center mb-3">-->
-<!--                            <div class="fw-bolder">Form submission successful!</div>-->
-<!--                            To activate this form, sign up at-->
-<!--                            <br />-->
-<!--                            <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                     Submit error message-->
-<!--                    -->
-<!--                     This is what your users will see when there is-->
-<!--                     an error submitting the form-->
-<!--                    <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>-->
-<!--                     Submit Button-->
-<!--                    <div class="d-grid"><button class="btn btn-primary rounded-pill btn-lg disabled" id="submitButton" type="submit">Submit</button></div>-->
-<!--                    <br>-->
-<!--                    <div class="form-floating mb-3" style="color: #5caa32">-->
-<!--                        <div class="modal-header">Already a User?</div>-->
-<!--                    </div>-->
-
-                    <!-- Sign Up Button-->
-<!--                    <div class="d-grid"><button class="btn btn-primary rounded-pill btn-lg disabled" id="signUpButton" type="submit" href="--><?php //echo yii\helpers\Url::to(['site/map']) ?><!--">Sign Up</button></div>-->
-<!--                </form>-->
-<!--            </div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
-
-<?php $this->endBody() ?>
-</body>
-</html>
+    </footer>
+    <?php $this->endBody() ?>
+    </body>
+    </html>
 <?php $this->endPage() ?>
--- tasos/GaiaPlatform/web/js/scripts.js
+++ tasos/GaiaPlatform/web/js/scripts.js
@@ -7,29 +7,74 @@
 // Scripts
 // 
 
-window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
+let config = {
+    minZoom: 7,
+    maxZoom: 18,
+    zoomControl: false
+};
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
+const zoom = 11;
 
+const lat = 39.6935;
+const lng = 20.8465;
+
+
+map = L.map("map_element", config).setView([lat, lng], zoom);
+
+LeafIcon = L.Icon.extend({
+    options: {
+        iconSize: [25, 30],
+        popupAnchor: [-1, -15]
+    }
 });
 
+Icon = new LeafIcon({
+    iconUrl: 'asset/stationGreen.png',
+})
+
+console.log('i am here');
+L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png', {
+    maxZoom: 20,
+}).addTo(map);
+
+marker3 = L.marker([39.6216, 20.8596], {icon: Icon}).addTo(map);
+
+
+marker4 = L.marker([39.7147, 20.7572], {icon: Icon}).addTo(map);
+
+marker5 = L.marker([39.7027, 20.8122], {icon: Icon}).addTo(map);
+
+marker6 = L.marker([39.7066, 20.7926], {icon: Icon}).addTo(map);
+
+var legend = L.control({ position: "topright" });
+
+map.attributionControl.setPrefix();
+
+legend.onAdd = function(map) {
+    var div = L.DomUtil.create("div", "legend_info");
+    div.innerHTML += "<div class='three'><h1>Case Study - Urban Air Quality</h1></div>";
+
+
+
+
+    return div;
+};
+
+legend.addTo(map);
+
+function onMapClick() {
+    window.location.href = locationMap;
+}
+
+map.on('click', onMapClick);
+
+
+legend.getContainer().addEventListener('mouseover', function () {
+    map.dragging.disable();
+});
+
+// Re-enable dragging when user's cursor leaves the element
+legend.getContainer().addEventListener('mouseout', function () {
+    map.dragging.enable();
+});
