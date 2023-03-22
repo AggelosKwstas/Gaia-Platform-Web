fb0333cfc3ec01b4b530a373e1e2ea9121838a7f
tasosvittas <tasosvittas@gmail.com>
2023-03-22 15:12:27 +0200

    zoom out image main

--- tasos/GaiaPlatform/assets/AppAsset.php
+++ tasos/GaiaPlatform/assets/AppAsset.php
@@ -30,7 +30,8 @@ class AppAsset extends AssetBundle
         'https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&amp;display=swap',
         'https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap',
         'css/styles.css',
-        'css/downloadButton.css'
+        'css/downloadButton.css',
+        'https://unpkg.com/aos@2.3.1/dist/aos.css'
     ];
 
     public $js = [
@@ -39,7 +40,8 @@ class AppAsset extends AssetBundle
         'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
         'https://cdn.startbootstrap.com/sb-forms-latest.js',
         'js/map_smallscreen.js',
-        'js/map_fullscreen.js'
+        'js/map_fullscreen.js',
+        'https://unpkg.com/aos@2.3.1/dist/aos.js'
     ];
 
     public $depends = [
--- tasos/GaiaPlatform/views/site/index.php
+++ tasos/GaiaPlatform/views/site/index.php
@@ -8,26 +8,15 @@ use yii\helpers\Url;
 $this->title = 'GAIA V2';
 $this->registerJs("let locationMap='" . Url::to(['site/map']) . "'", \yii\web\View::POS_BEGIN);
 ?>
-
-<header class="masthead">
-    <div class="container px-5">
-        <div class="item" data-aos="fade-up">1</div>
-        <div class="item" data-aos="fade-down">2</div>
-        <div class="item" data-aos="fade-right">3</div>
-        <div class="item" data-aos="fade-left">4</div>
-
-        <div class="item" data-aos="zoom-in">5</div>
-        <div class="item" data-aos="zoom-out">6</div>
-
-        <div class="item" data-aos="slide-up">7</div>
-
-        <div class="item" data-aos="flip-up">8</div>
-        <div class="item" data-aos="flip-down">9</div>
-        <div class="item" data-aos="flip-right">10</div>
-        <div class="item" data-aos="flip-left">11</div>
+<header>
+    <div class="container-zoom">
+        <div class="item-zoom">
+            <img src="asset/airPolution3.jpg" style="width:100%">
+        </div>
 
     </div>
 </header>
+
 
 
 
