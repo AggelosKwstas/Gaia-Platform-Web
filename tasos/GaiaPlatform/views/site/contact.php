37937ad35e19a2dfdc74a4c76e5a453a6fbeb026
tasosvittas <tasosvittas@gmail.com>
2023-04-04 13:51:35 +0300

    without contact us

--- tasos/GaiaPlatform/views/site/contact.php
+++ tasos/GaiaPlatform/views/site/contact.php
@@ -99,4 +99,70 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php endif; ?>
 </div>
 </section>
+<!DOCTYPE html>
+<!-- Created By CodingLab - www.codinglabweb.com -->
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <!-- <title> Responsive Contact Us Form  | CodingLab </title>-->
+    <link rel="stylesheet" href="style.css">
+    <!-- Fontawesome CDN Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div class="container-contact">
+    <div class="content">
+        <div class="left-side">
+            <div class="address details">
+                <i class="fas fa-map-marker-alt"></i>
+                <div class="topic">Address</div>
+                <div class="text-one">Science and Technology Park of Epirus, University Campus</div>
+                <div class="text-two">P.C. 45 110, Ioannina, Greece</div>
+            </div>
+            <div class="phone details">
+                <i class="fas fa-phone-alt"></i>
+                <div class="topic">Phone</div>
+                <div class="text-one">+30 26515 00012</div>
+            </div>
+            <div class="fax details">
+                <i class="fas fa-fax"></i>
+                <div class="topic">Fax</div>
+                <div class="text-one">+30 26510 40637</div>
+            </div>
+            <div class="email details">
+                <i class="fas fa-envelope"></i>
+                <div class="topic">Email</div>
+                <div class="text-one">info@neuronenergy.com</div>
+            </div>
+        </div>
+        <div class="right-side">
+            <div class="topic-text">Send us a message</div>
+            <p>If you have any work from me or any types of quries related to my tutorial, you can send me message from here. It's my pleasure to help you.</p>
+            <form action="#">
+                <div class="input-box">
+                    <input type="text" placeholder="Enter your name">
+                </div>
+                <div class="input-box">
+                    <input type="text" placeholder="Enter your email">
+                </div>
+                <div class="input-box">
+                    <input type="text" placeholder="Enter your subject">
+                </div>
+                <div class="input-box">
+                    <input type="text" placeholder="Enter your comment">
+                </div>
+                <div class="input-box message-box">
+
+                </div>
+                <div class="button">
+                    <input type="button" value="Send Now" >
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
+
 
