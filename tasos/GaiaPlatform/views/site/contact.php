ead136c97c2a622968a89b35bde4e68b04531ed8
tasosvittas <tasosvittas@gmail.com>
2023-04-04 17:01:53 +0300

    without contact us

--- tasos/GaiaPlatform/views/site/contact.php
+++ tasos/GaiaPlatform/views/site/contact.php
@@ -11,106 +11,96 @@ use yii\captcha\Captcha;
 $this->title = 'Contact';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-
-<section class="contact-layout">
-    <div class="container" style="text-align: center">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
-
-        <div class="alert alert-success">
-            Thank you for contacting us. We will respond to you as soon as possible.
-        </div>
-
-        <p>
-            Note that if you turn on the Yii debugger, you should be able
-            to view the mail message on the mail panel of the debugger.
-            <?php if (Yii::$app->mailer->useFileTransport): ?>
-                Because the application is in development mode, the email is not sent but saved as
-                a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
-                Please configure the <code>useFileTransport</code> property of the <code>mail</code>
-                application component to be false to enable email sending.
-            <?php endif; ?>
-        </p>
-
-    <?php else: ?>
-
-        <p>
-            If you have business inquiries or other questions, please fill out the following form to contact us.
-            Thank you.
-        </p>
-        <div class="login-wrap p-4 p-md-5">
-        <div class="row">
-            <div class="col ">
-
-                <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
-
-                    <?= $form->field($model, 'name', [
-                        'inputOptions' => [
-                            'class' => 'form-control form-control-user radius-contact',
-                            'placeholder' => 'Enter your full name'
-
-                        ]
-                    ])->textInput(['autofocus' => true]) ?>
-
-                    <?= $form->field($model, 'email',
-                        [
-                            'inputOptions' => [
-                                'class' => 'form-control form-control-user radius-contact',
-                                'placeholder' => 'Enter your email'
-                            ]
-                        ]) ?>
-
-                    <?= $form->field($model, 'subject',
-                        [
-                            'inputOptions' => [
-                                'class' => 'form-control form-control-user radius-contact',
-                                'placeholder' => 'Enter subject name'
-                            ]
-                        ]) ?>
-
-                    <?= $form->field($model, 'body',
-                        [
-                            'inputOptions' => [
-                                'class' => 'form-control form-control-user radius-contact',
-                                'placeholder' => 'Enter your comment'
-                            ]
-                        ])->textarea(['rows' => 6]) ?>
-
-                    <?= $form->field($model, 'verifyCode',[
-                        'inputOptions' => [
-                            'class' => 'form-control form-control-user radius-contact',
-                            'placeholder' => 'Prove you are not a robot'
-                        ]
-                    ])->widget(Captcha::class, [
-                        'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-                    ]) ?>
-
-                    <div class="form-group">
-                        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-                    </div>
-
-                <?php ActiveForm::end(); ?>
-
-<!--            </div>-->
-        </div>
-        </div>
-    <?php endif; ?>
-</div>
-</section>
-<!DOCTYPE html>
+<!---->
+<!--<section class="contact-layout">-->
+<!--    <div class="container" style="text-align: center">-->
+<!---->
+<!--    <h1>--><?php //= Html::encode($this->title) ?><!--</h1>-->
+<!---->
+<!--    --><?php //if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
+<!---->
+<!--        <div class="alert alert-success">-->
+<!--            Thank you for contacting us. We will respond to you as soon as possible.-->
+<!--        </div>-->
+<!---->
+<!--        <p>-->
+<!--            Note that if you turn on the Yii debugger, you should be able-->
+<!--            to view the mail message on the mail panel of the debugger.-->
+<!--            --><?php //if (Yii::$app->mailer->useFileTransport): ?>
+<!--                Because the application is in development mode, the email is not sent but saved as-->
+<!--                a file under <code>--><?php //= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?><!--</code>.-->
+<!--                Please configure the <code>useFileTransport</code> property of the <code>mail</code>-->
+<!--                application component to be false to enable email sending.-->
+<!--            --><?php //endif; ?>
+<!--        </p>-->
+<!---->
+<!--    --><?php //else: ?>
+<!---->
+<!--        <p>-->
+<!--            If you have business inquiries or other questions, please fill out the following form to contact us.-->
+<!--            Thank you.-->
+<!--        </p>-->
+<!--        <div class="login-wrap p-4 p-md-5">-->
+<!--        <div class="row">-->
+<!--            <div class="col ">-->
+<!---->
+<!--                --><?php //$form = ActiveForm::begin(['id' => 'contact-form']); ?>
+<!---->
+<!--<!--                    -->--><?php ////= $form->field($model, 'name', [
+////                        'inputOptions' => [
+////                            'class' => 'form-control form-control-user radius-contact',
+////                            'placeholder' => 'Enter your full name'
+////
+////                        ]
+////                    ])->textInput(['autofocus' => true]) ?>
+<!---->
+<!--<!--                    -->--><?php ////= $form->field($model, 'email',
+////                        [
+////                            'inputOptions' => [
+////                                'class' => 'form-control form-control-user radius-contact',
+////                                'placeholder' => 'Enter your email'
+////                            ]
+////                        ]) ?>
+<!---->
+<!--                    --><?php //= $form->field($model, 'subject',
+//                        [
+//                            'inputOptions' => [
+//                                'class' => 'form-control form-control-user radius-contact',
+//                                'placeholder' => 'Enter subject name'
+//                            ]
+//                        ]) ?>
+<!---->
+<!--                    --><?php //= $form->field($model, 'body',
+//                        [
+//                            'inputOptions' => [
+//                                'class' => 'form-control form-control-user radius-contact',
+//                                'placeholder' => 'Enter your comment'
+//                            ]
+//                        ])->textarea(['rows' => 6]) ?>
+<!---->
+<!--                    --><?php //= $form->field($model, 'verifyCode',[
+//                        'inputOptions' => [
+//                            'class' => 'form-control form-control-user radius-contact',
+//                            'placeholder' => 'Prove you are not a robot'
+//                        ]
+//                    ])->widget(Captcha::class, [
+//                        'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
+//                    ]) ?>
+<!---->
+<!--                    <div class="form-group">-->
+<!--                        --><?php //= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+<!--                    </div>-->
+<!---->
+<!--                --><?php //ActiveForm::end(); ?>
+<!---->
+<!--<!--            </div>-->-->
+<!--        </div>-->
+<!--        </div>-->
+<!--    --><?php //endif; ?>
+<!--</div>-->
+<!--</section>-->
 <!-- Created By CodingLab - www.codinglabweb.com -->
-<html lang="en" dir="ltr">
-<head>
-    <meta charset="UTF-8">
-    <!-- <title> Responsive Contact Us Form  | CodingLab </title>-->
-    <link rel="stylesheet" href="style.css">
-    <!-- Fontawesome CDN Link -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
+
 <div class="container-contact">
     <div class="content">
         <div class="left-side">
@@ -139,30 +129,59 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="right-side">
             <div class="topic-text">Send us a message</div>
             <p>If you have any work from me or any types of quries related to my tutorial, you can send me message from here. It's my pleasure to help you.</p>
-            <form action="#">
-                <div class="input-box">
-                    <input type="text" placeholder="Enter your name">
-                </div>
-                <div class="input-box">
-                    <input type="text" placeholder="Enter your email">
-                </div>
-                <div class="input-box">
-                    <input type="text" placeholder="Enter your subject">
-                </div>
-                <div class="input-box">
-                    <input type="text" placeholder="Enter your comment">
-                </div>
+            <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
+
+
+                <?= $form->field($model, 'name', [
+                    'inputOptions' => [
+                        'class' => 'form-control form-control-user input-box',
+                        'placeholder' => 'Enter your name'
+
+                    ]
+                ])->textInput(['autofocus' => true]) ?>
+<!--                <div class="input-box">-->
+<!--                    <input type="text" placeholder="Enter your name">-->
+<!--                </div>-->
+
+                <?= $form->field($model, 'email',
+                [
+                    'inputOptions' => [
+                        'class' => 'form-control form-control-user input-box',
+                        'placeholder' => 'Enter your email'
+                    ]
+                ]) ?>
+<!--                <div class="input-box">-->
+<!--                    <input type="text" placeholder="Enter your email">-->
+<!--                </div>-->
+            <?= $form->field($model, 'subject',
+                [
+                    'inputOptions' => [
+                        'class' => 'form-control form-control-user input-box',
+                        'placeholder' => 'Enter subject'
+                    ]
+                ]) ?>
+<!--                <div class="input-box">-->
+<!--                    <input type="text" placeholder="Enter your subject">-->
+<!--                </div>-->
+            <?= $form->field($model, 'body',
+                [
+                    'inputOptions' => [
+                        'class' => 'form-control form-control-user radius-contact',
+                        'placeholder' => 'Enter your comment'
+                    ]
+                ])->textarea(['rows' => 6]) ?>
+<!--                <div class="input-box">-->
+<!--                    <input type="text" placeholder="Enter your comment">-->
+<!--                </div>-->
                 <div class="input-box message-box">
 
                 </div>
                 <div class="button">
-                    <input type="button" value="Send Now" >
+                    <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
                 </div>
-            </form>
+            <?php ActiveForm::end(); ?>
         </div>
     </div>
 </div>
-</body>
-</html>
 
 
