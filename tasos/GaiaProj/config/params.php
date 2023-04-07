c6c1cd00c06a5ec2b7af65eff0ede60e1f65e7fe
tasosvittas <tasosvittas@gmail.com>
2023-04-07 11:18:11 +0300

    contact us

---
+++ tasos/GaiaProj/config/params.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'adminEmail' => 'tasostest.smtp@gmail.com',
+    'senderEmail' => 'tasostest.smtp@gmail.com',
+    'ccEmail' => 'tasosvittas@gmail.com',
+    'senderName' => 'Test',
+];
