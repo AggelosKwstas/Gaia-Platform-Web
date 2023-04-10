b54abe2a6ad251acf7a7015da034dc77b94b8829
unknown <agelos.kostas21@gmail.com>
2023-04-10 15:28:59 +0300

    better if statement for mobile

--- aggelos/GaiaPlatform/web/js/map_fullscreen.js
+++ aggelos/GaiaPlatform/web/js/map_fullscreen.js
@@ -268,7 +268,14 @@ legend.getContainer().addEventListener('mouseout', function () {
 map2.doubleClickZoom.disable();
 
 //close legend when popup is open on small devices.
-if (window.innerWidth <= 700) {
+if (navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)) {
+    console.log('mobile')
     map2.on('popupopen', () => {
         $('.legend').hide();
     })
